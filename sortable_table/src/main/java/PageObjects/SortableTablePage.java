c42c63f63c3964e1328b67c004b486360656acae
TayKieu <kieuduongtay@gmail.com>
2025-05-29 08:01:49 +0700

    complete VP2

--- sortable_table/src/main/java/PageObjects/SortableTablePage.java
+++ sortable_table/src/main/java/PageObjects/SortableTablePage.java
@@ -7,7 +7,9 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class SortableTablePage extends GeneralPage {
     private final By rowsXpath = By.xpath("//table[@id='table1']/tbody/tr");
@@ -20,16 +22,10 @@ public class SortableTablePage extends GeneralPage {
         return By.xpath(xpath);
     }
 
-    protected WebElement getHeader(String header){
-        return Constant.webDriver.findElement(headerXpath(header));
-    }
 
-    protected List<WebElement> getRowsOfTable1(){
-        return Constant.webDriver.findElements(rowsXpath);
-    }
     public List<TableData> getAllDataOfTable1(){
         List<TableData> tableDataList = new ArrayList<>();
-        List<WebElement> rows = getRowsOfTable1();
+        List<WebElement> rows = Constant.webDriver.findElements(rowsXpath);
         for(int i = 0; i < rows.size(); i++){
             List<WebElement> cols = rows.get(i).findElements(By.tagName("td"));
             String lastName = cols.get(0).getText();
@@ -43,13 +39,63 @@ public class SortableTablePage extends GeneralPage {
         }
         return tableDataList;
     }
+    public TableData getFirstRowData(){
+        return getAllDataOfTable1().get(0);
+    }
 
     public void clickHeader (SortableDataTableHeader header){
-        getHeader(header.getHeader()).click();
+        Constant.webDriver.findElement(headerXpath(header.getHeader())).click();
     }
 
     public boolean isColumnSorted(SortableDataTableHeader header){
-        getHeader(header.getHeader()).click();
-        return true;
+        clickHeader(header);
+        boolean sortedAsc = isSortedAsc(header);
+        clickHeader(header);
+        boolean sortedDesc = isSortedDesc(header);
+        return sortedAsc && sortedDesc;
+    }
+
+    public List<String> getColumnValues(SortableDataTableHeader header){
+        List<String> colValues = new ArrayList<>();
+        List<WebElement> rows = Constant.webDriver.findElements(rowsXpath);
+        for(int i = 0; i < rows.size(); i++){
+            String values = Constant.webDriver.findElement(colsDataXpath(header.getColIndex())).getText();
+            colValues.add(values);
+        }
+        return colValues;
+    }
+    public boolean isSortedAsc(SortableDataTableHeader header) {
+        List<String> originalValues = getColumnValues(header);
+
+        if (header == SortableDataTableHeader.DUE) {
+            List<Double> original = originalValues.stream()
+                    .map(val -> Double.parseDouble(val.replace("$", "")))
+                    .collect(Collectors.toList());
+
+            List<Double> sorted = new ArrayList<>(original);
+            Collections.sort(sorted);
+            return original.equals(sorted);
+        } else {
+            List<String> sorted = new ArrayList<>(originalValues);
+            Collections.sort(sorted);
+            return originalValues.equals(sorted);
+        }
+    }
+    public boolean isSortedDesc(SortableDataTableHeader header) {
+        List<String> originalValues = getColumnValues(header);
+
+        if (header == SortableDataTableHeader.DUE) {
+            List<Double> original = originalValues.stream()
+                    .map(val -> Double.parseDouble(val.replace("$", "")))
+                    .collect(Collectors.toList());
+
+            List<Double> sorted = new ArrayList<>(original);
+            sorted.sort(Collections.reverseOrder());
+            return original.equals(sorted);
+        } else {
+            List<String> sorted = new ArrayList<>(originalValues);
+            sorted.sort(Collections.reverseOrder());
+            return originalValues.equals(sorted);
+        }
     }
 }
--- sortable_table/src/test/java/TestCases/SortableDataTableTest.java
+++ sortable_table/src/test/java/TestCases/SortableDataTableTest.java
@@ -12,20 +12,33 @@ import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
+
+import java.util.List;
 
 public class SortableDataTableTest {
     //ExtentTest extentTest;
-    TableData tableData;
+    TableData actualFirstRow;
+    TableData expectedFirstRow;
     SortableTablePage sortableTablePage;
     HomePage homePage;
+    SoftAssert softAssert;
     @BeforeMethod
     public void beforeMethod(){
         System.out.println("Pre-condition");
 
         Constant.webDriver = new ChromeDriver();
         Constant.webDriver.manage().window().maximize();
-        tableData = new TableData();
+        actualFirstRow = new TableData();
+        expectedFirstRow = new TableData(
+                "Smith",
+                "John",
+                "jsmith@gmail.com",
+                "$50.00",
+                "http://www.jsmith.com"
+        );
         sortableTablePage = new SortableTablePage();
+        softAssert = new SoftAssert();
         homePage = new HomePage();
         homePage.open();
     }
@@ -47,23 +60,23 @@ public class SortableDataTableTest {
     @Test(description = "Verify that the first table has correct data in the first row")
     public void TC1(){
         homePage.goToPage(PageName.SORTABLE_DATA_TABLE);
-        sortableTablePage.getAllDataOfTable1();
-        TableData firstRow =  sortableTablePage.getAllDataOfTable1().get(0);
-        Assert.assertEquals("Smith", firstRow.getLastName());
-        Assert.assertEquals("John", firstRow.getFirstName());
-        Assert.assertEquals("jsmith@gmail.com", firstRow.getEmail());
-        Assert.assertEquals("$50.00", firstRow.getDue());
-        Assert.assertEquals("http://www.jsmith.com", firstRow.getWebsite());
+        actualFirstRow =  sortableTablePage.getFirstRowData();
+        softAssert.assertEquals(actualFirstRow.getLastName(), expectedFirstRow.getLastName(), "Last name mismatch");
+        softAssert.assertEquals(actualFirstRow.getFirstName(), expectedFirstRow.getFirstName(), "First name mismatch");
+        softAssert.assertEquals(actualFirstRow.getEmail(), expectedFirstRow.getEmail(), "Email mismatch");
+        softAssert.assertEquals(actualFirstRow.getDue(), expectedFirstRow.getDue(), "Due mismatch");
+        softAssert.assertEquals(actualFirstRow.getWebsite(), expectedFirstRow.getWebsite(), "Website mismatch");
+        softAssert.assertAll();
     }
 
     @Test(description = "Verify the data in the columns is sorted ASC/DESC when click on the header of each column")
     public void TC2(){
         homePage.goToPage(PageName.SORTABLE_DATA_TABLE);
-        sortableTablePage.clickHeader(SortableDataTableHeader.LAST_NAME);
-        sortableTablePage.clickHeader(SortableDataTableHeader.FIRST_NAME);
-        sortableTablePage.clickHeader(SortableDataTableHeader.EMAIL);
-        sortableTablePage.clickHeader(SortableDataTableHeader.DUE);
-        sortableTablePage.clickHeader(SortableDataTableHeader.WEBSITE);
-        sortableTablePage.clickHeader(SortableDataTableHeader.LAST_NAME);
+        softAssert.assertTrue(sortableTablePage.isColumnSorted(SortableDataTableHeader.LAST_NAME));
+        softAssert.assertTrue(sortableTablePage.isColumnSorted(SortableDataTableHeader.FIRST_NAME));
+        softAssert.assertTrue(sortableTablePage.isColumnSorted(SortableDataTableHeader.EMAIL));
+        softAssert.assertTrue(sortableTablePage.isColumnSorted(SortableDataTableHeader.DUE));
+        softAssert.assertTrue(sortableTablePage.isColumnSorted(SortableDataTableHeader.WEBSITE));
+        softAssert.assertAll();
     }
 }
